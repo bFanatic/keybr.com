70734ccacc65f2a27d88cf233d4d9399733f610b
fanne <info@bfanatic.be>
2026-03-20 21:22:44 +0100

    deploy issues

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 @echo off
 set DEST=\\truenas.local\docker\keybr.com
 
-robocopy . "%DEST%" /E /XD node_modules .git /NFL /NDL /NJH /NJS
+robocopy . "%DEST%" Dockerfile docker-compose.yaml dashboard.Dockerfile dashboard.ts .env /NFL /NDL /NJH /NJS
